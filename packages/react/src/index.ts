85356159a63c73b8fc1d628c8035e2f2aceae3a8
Jeroen Claassens <support@favware.tech>
2020-09-20 16:56:59 +0200

    chore: version bumps

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,9 +3,8 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from '@favware/syntax-highlighter-core';
+import type { JSX } from '@favware/syntax-highlighter-core';
 
-import { defineCustomElements, applyPolyfills } from '@favware/syntax-highlighter-core/loader';
 
-applyPolyfills().then(() => defineCustomElements());
+
 export const SyntaxHighlighter = /*@__PURE__*/createReactComponent<JSX.SyntaxHighlighter, HTMLSyntaxHighlighterElement>('syntax-highlighter');
